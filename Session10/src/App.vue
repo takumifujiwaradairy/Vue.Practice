348a9f22e3acc29e23949ecd8dd50ee758cf5fa2
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-07 21:57:49 +0900

    demoapp3-2

--- Session10/src/App.vue
+++ Session10/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resource 
+    v-for="res in storedResources" 
+    :key="res.id" 
+    :title="res.title" 
+    :description="res.description"
+    :link="res.link">
+    </learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learing-resources/LearningResource.vue';
+
 export default {
+  components:{
+    LearningResource:LearningResource
+  },
   data() {
     return {
-      storedResource:[
+      storedResources:[
         {id: 'official-guide',
         title: 'Official Guide',
         description: 'The official Vue.js documentation.',
