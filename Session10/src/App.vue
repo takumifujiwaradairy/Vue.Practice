4b0cc873310cd971fa38a0d36a25ffedaec8cd44
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-07 21:07:36 +0900

    demoapp3-1

---
+++ Session10/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResource:[
+        {id: 'official-guide',
+        title: 'Official Guide',
+        description: 'The official Vue.js documentation.',
+        link: 'https://vuejs.org'
+        },
+        {id: 'Goggle',
+        title: 'Goggle',
+        description: 'Learn to google...',
+        link: 'https://google.org'
+        },
+      ]
+    };
+  }
+}
+</script>
