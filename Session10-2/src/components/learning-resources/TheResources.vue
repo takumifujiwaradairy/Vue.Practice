7a60ca11b9940c0ad32abb418af57e0d3523f406
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-08 13:51:08 +0900

    demoapp3-6

---
+++ Session10-2/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,48 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+  import StoredResources from "./Stored-Resources.vue";
+  import AddResource from "./Add-resource.vue";
+
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data(){
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  provide(){
+    return {
+      resources: this.storedResources
+    };
+  },
+  methods:{
+    setSelectedTab(tab){
+      this.setSelectedTab = tab;
+    }
+  }
+}
+</script>
