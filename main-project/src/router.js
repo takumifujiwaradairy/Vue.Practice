4254db80f295895892e89e0174f6df6e8dfaa339
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-10 16:03:41 +0900

    MP3

--- main-project/src/router.js
+++ main-project/src/router.js
@@ -1,16 +1,23 @@
 import { createRouter } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistation from './pages/coaches/CoachesRegistation.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routers:[
     {path: '/', redirect:'/coaches'},
-    {path: '/coaches', component: null},
-    {path: '/coaches/:id', component: null, children:[
-      {path: '/contact', component: null}
+    {path: '/coaches', component: CoachesList},
+    {path: '/coaches/:id', component: CoachDetail, children:[
+      {path: '/contact', component: ContactCoach}
     ]},
-    {path: '/register', component: null},
-    {path: '/requests', component: null},
-    {path: '/:notFound(.*)',component: null},
+    {path: '/register', component: CoachRegistation},
+    {path: '/requests', component: RequestsReceived},
+    {path: '/:notFound(.*)',component: NotFound},
   ],
 });
 
