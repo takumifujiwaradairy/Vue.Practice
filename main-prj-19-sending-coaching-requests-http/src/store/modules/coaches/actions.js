8ba70984ce82125781164ac2a6deede1747251cf
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-11 16:22:44 +0900

    MP14

--- main-prj-19-sending-coaching-requests-http/src/store/modules/coaches/actions.js
+++ main-prj-19-sending-coaching-requests-http/src/store/modules/coaches/actions.js
@@ -10,7 +10,7 @@ export default {
     };
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches/${userId}.json`,
+      `https://democoachfind-default-rtdb.firebaseio.com//coaches/${userId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
@@ -28,9 +28,13 @@ export default {
       id: userId
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if(!payload.forceRefresh && !context.getters.shouldUpdate){
+      return;
+    }
+
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches.json`
+      `https://democoachfind-default-rtdb.firebaseio.com//coaches.json`
     );
     const responseData = await response.json();
 
@@ -54,5 +58,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   }
 };
