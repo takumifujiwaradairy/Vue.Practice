5c1e6172bdd19b4d7994c9defcddcd6d9a04f5ff
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-11 15:59:44 +0900

    MP13

--- main-prj-13-storing-requests-messages/src/store/modules/coaches/actions.js
+++ main-prj-13-storing-requests-messages/src/store/modules/coaches/actions.js
@@ -1,14 +1,27 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     };
+    const response = await fetch(`https://democoachfind-default-rtdb.firebaseio.com/coaches/${userId}.json`,{
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
 
-    context.commit('registerCoach', coachData);
+    // const responseData = await response.json();
+
+    if(!response.ok){
+
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
