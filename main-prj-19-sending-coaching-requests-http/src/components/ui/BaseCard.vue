f720d9422da12d3aace4a581d8397f258f7f7037
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-13 15:15:34 +0900

    SP2

--- main-prj-19-sending-coaching-requests-http/src/components/ui/BaseCard.vue
+++ main-prj-19-sending-coaching-requests-http/src/components/ui/BaseCard.vue
@@ -6,12 +6,11 @@
 lv
 <style scoped>
 .card {
-  cpscodeのsetting.jsonにｋれを追加すれば、
-vvとjjが使えるようになりますので。
-入れてください！border-radius: 12px;
+  border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
   margin: 2rem auto;
   max-width: 40rem;
 }
+
 </style>
---
+++ prj-cmp-01-starting-setup 2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+aaaadd
+  <ul v-for="res in storedResources" :key="res.id" :title="res.title" :link="res.link">
+    <resource-item>
+    </resource-item>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './components/ResourceItem.vue';
+export default {
+  components:{
+    ResourceItem
+  },
+  data(){
+    return{
+      storedResources: [{
+        id: 'offical-guide',
+        titie:'Official Guide',
+        description:'The official Guide',
+        link: 'https://vuejs.org'
+      },
+      {
+        id: 'offical-guide',
+        titie:'Official Guide',
+        description:'The official Guide',
+        link: 'https://vuejs.org'
+      }]
+    };
+  }
+}
+</script>
