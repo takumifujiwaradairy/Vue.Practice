fd134a63131bbd062953a0cb4c933c43898f5317
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-14 13:44:54 +0900

    FP2

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,14 +1,22 @@
 import { createRouter,createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistation from './pages/coaches/CoachesRegistation.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {path: '/', redirect: '/coaches'},
-    { path: '/coaches', component: null},
-    { path: '/coaches/:id', component: null,children:[
-     {path: 'contact',component: null}, 
+    { path: '/coaches', component: CoachesList},
+    { path: '/coaches/:id', component: CoachDetail,children:[
+     {path: 'contact',component: ContactCoach}, 
     ]},
-    { path: '/register', component: null},
+    { path: '/register', component: CoachRegistation},
+    { path: '/requests', component: RequestsReceived},
     { path: '/:notFound(.*)', component: null},
   ],
 });
