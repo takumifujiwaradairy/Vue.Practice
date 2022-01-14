ae81939d93a644a1aad917a668e579162cd3064c
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-14 14:19:12 +0900

    FP3

---
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    FILETR
+  </section>
+  <section>
+    LIST OF COACHES
+  </section>
+</template>
--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -2,10 +2,10 @@ import { createRouter,createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachesRegistation.vue';
+import CoachRegistation from './pages/coaches/CoachRegistation.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import NotFound from './pages/NotFound.vue';
+
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,9 +15,8 @@ const router = createRouter({
     { path: '/coaches/:id', component: CoachDetail,children:[
      {path: 'contact',component: ContactCoach}, 
     ]},
-    { path: '/register', component: CoachRegistation},
-    { path: '/requests', component: RequestsReceived},
-    { path: '/:notFound(.*)', component: null},
+     { path: '/register', component: CoachRegistation},
+     { path: '/requests', component: RequestsReceived},
   ],
 });
 
